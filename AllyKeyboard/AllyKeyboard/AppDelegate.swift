97b60833152e9c5581a5bbec1a54b5814a75ef1e
umkasanki <umkasanki@gmail.com>
2026-05-23 18:54:29 +0200

    Add Xcode project skeleton (AppKit, Storyboard)

---
+++ AllyKeyboard/AllyKeyboard/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AllyKeyboard
+//
+//  Created by user945037 on 5/23/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
